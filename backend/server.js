2daf71ea941adad14bc73c2201087e64739a6fc7
Pawan <filtermanapr259@gmail.com>
2026-06-05 20:51:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,7 @@ app.use(
       "https://av-mern.vercel.app/"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
   })
 );
